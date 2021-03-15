9ef07f2831762240b50dd5a82e114a64ff00e499
Dave White <deivid77@gmail.com>
2021-03-15 13:44:28 +0100

    Export categories to CSV added

--- GloboTicket.TicketManagement/GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
@@ -1,7 +1,7 @@
 ﻿using AutoMapper;
-//using GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
+using GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
 using GloboTicket.TicketManagement.Application.Contracts.Persistence;
-//using GloboTicket.TicketManagement.Application.Models.Mail;
+using GloboTicket.TicketManagement.Application.Models.Mail;
 using GloboTicket.TicketManagement.Domain.Entities;
 using MediatR;
 //using Microsoft.Extensions.Logging;
@@ -15,15 +15,15 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
     {
         private readonly IEventRepository _eventRepository;
         private readonly IMapper _mapper;
-        //private readonly IEmailService _emailService;
+        private readonly IEmailService _emailService;
         //private readonly ILogger<CreateEventCommandHandler> _logger;
 
 
-        public CreateEventCommandHandler(IMapper mapper, IEventRepository eventRepository) //, IEmailService emailService, ILogger<CreateEventCommandHandler> logger)
+        public CreateEventCommandHandler(IMapper mapper, IEventRepository eventRepository, IEmailService emailService) //, ILogger<CreateEventCommandHandler> logger)
         {
             _mapper = mapper;
             _eventRepository = eventRepository;
-            //_emailService = emailService;
+            _emailService = emailService;
             //_logger = logger;
         }
 
@@ -39,18 +39,18 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
 
             @event = await _eventRepository.AddAsync(@event);
 
-            ////Sending email notification to admin address
-            //var email = new Email() { To = "gill@snowball.be", Body = $"A new event was created: {request}", Subject = "A new event was created" };
+            //Sending email notification to admin address
+            var email = new Email() { To = "deivid77@gmail.com", Body = $"A new event was created: {request}", Subject = "A new event was created" };
 
-            //try
-            //{
-            //    await _emailService.SendEmail(email);
-            //}
-            //catch (Exception ex)
-            //{
-            //    //this shouldn't stop the API from doing else so this can be logged
-            //    _logger.LogError($"Mailing about event {@event.EventId} failed due to an error with the mail service: {ex.Message}");
-            //}
+            try
+            {
+                await _emailService.SendEmail(email);
+            }
+            catch (Exception ex)
+            {
+                //this shouldn't stop the API from doing else so this can be logged
+                //_logger.LogError($"Mailing about event {@event.EventId} failed due to an error with the mail service: {ex.Message}");
+            }
 
             return @event.EventId;
         }
--- GloboTicket.TicketManagement/GloboTicket.TicketManagement.Application/Profiles/MappingProfile.cs
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Application/Profiles/MappingProfile.cs
@@ -5,6 +5,7 @@ using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCa
 using GloboTicket.TicketManagement.Application.Features.Events.Commands.CreateEvent;
 using GloboTicket.TicketManagement.Application.Features.Events.Commands.UpdateEvent;
 using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventDetail;
+using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsExport;
 using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsList;
 //using GloboTicket.TicketManagement.Application.Features.Orders.GetOrdersForMonth;
 using GloboTicket.TicketManagement.Domain.Entities;
@@ -19,7 +20,8 @@ namespace GloboTicket.TicketManagement.Application.Profiles
             CreateMap<Event, CreateEventCommand>().ReverseMap();
             CreateMap<Event, UpdateEventCommand>().ReverseMap();
             CreateMap<Event, EventDetailVm>().ReverseMap();
-            //CreateMap<Event, CategoryEventDto>().ReverseMap();
+            CreateMap<Event, CategoryEventDto>().ReverseMap();
+            CreateMap<Event, EventExportDto>().ReverseMap();
 
             CreateMap<Category, CategoryDto>();
             CreateMap<Category, CategoryListVm>();
