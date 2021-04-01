e544273ab39cf4a717f11764c487ca378b211989
Dave White <deivid77@gmail.com>
2021-04-01 15:42:46 +0200

    A little tweak to get it working

--- GloboTicket.TicketManagement/GloboTicket.TicketManagement.App/Program.cs
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.App/Program.cs
@@ -18,7 +18,7 @@ namespace GloboTicket.TicketManagement.App
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
             builder.Services.AddBlazoredLocalStorage();
@@ -27,12 +27,12 @@ namespace GloboTicket.TicketManagement.App
             builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 
 
-            builder.Services.AddSingleton(new HttpClient
+            builder.Services.AddScoped(sp => new HttpClient
             {
-                BaseAddress = new Uri("https://localhost:5001")
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
             });
 
-            builder.Services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri("https://localhost:5001"));
+            builder.Services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri("https://localhost:44331"));
 
             builder.Services.AddScoped<IEventDataService, EventDataService>();
             builder.Services.AddScoped<ICategoryDataService, CategoryDataService>();
